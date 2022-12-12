04dae65682580fcbc7eeb095b73be1bab4f2f97a
KeziaWahome <k.wahome@alustudent.com>
2022-12-12 22:37:49 +0200

    Backend login/logout API

--- signup_login_logout/routes/projectRoute.js
+++ signup_login_logout/routes/projectRoute.js
@@ -10,6 +10,7 @@ const {
 const { protect } = require('../middlewares/authmw')
 
 router.route('/').get(protect, getproject).post(protect, addproject)
+
 router.route('/:id').put(protect, updateproject)
 
 module.exports = router
